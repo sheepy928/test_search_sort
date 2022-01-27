d06efdf634ebe3643f4955183bcbcb40fdd3b5e5
sheepy928 <sheepy928@gmail.com>
2022-01-27 10:38:59 -0500

    Initial commit

---
+++ search.py
@@ -0,0 +1,152 @@
+import random
+import time
+from matplotlib import pyplot as plt
+
+# generate a list of random integers
+def generate_list(size):
+    l = []
+    for i in range(size):
+        l.append(random.randint(0, size))
+    return l
+
+# linear search
+def linear_search(l, key):
+    for i in range(len(l)):
+        if l[i] == key:
+            return i
+    return -1
+
+# iterative binary search of a sorted list
+def binary_search(l, key):
+    low = 0
+    high = len(l) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if l[mid] == key:
+            return mid
+        elif l[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+# # recursive binary search of a sorted sorted list
+# def recursive_binary_search(l, key):
+#     if len(l) == 0:
+#         return -1
+#     else:
+#         mid = len(l) // 2
+#         if l[mid] == key:
+#             return mid
+#         elif l[mid] < key:
+#             return recursive_binary_search(l[mid+1:], key)
+#         else:
+#             return recursive_binary_search(l[:mid], key)
+
+# run the search algorithms
+def search(list, key, method):
+    if method == 'linear':
+        return linear_search(list, key)
+    elif method == 'binary':
+        return binary_search(list, key)
+    # elif method == 'recursive':
+    #     return recursive_binary_search(list, key)
+    else:
+        return -1
+
+# randomly choose one number if the list
+def choose_key(list):
+    return list[random.randint(0, len(list) - 1)]
+
+# test1: running linear search on an unsorted and a sorted list
+def run_test1(num, cycle = 1000):
+    unsorted_list = generate_list(num)
+    sorted_list = sorted(unsorted_list)
+
+    print("Running linear search on an unsorted and a sorted list...")
+    time_unsorted = []
+    time_sorted = []
+    for _ in range(cycle):
+        key = choose_key(unsorted_list)
+
+        start = time.time()
+        index = search(unsorted_list, key, 'linear')
+        end = time.time()
+        time_unsorted.append(end - start)
+
+        # print("Running linear search on sorted_list1...")
+        start = time.time()
+        index = search(sorted_list, key, 'linear')
+        end = time.time()
+        time_sorted.append(end - start)
+
+    print("\tTotal time for linear search on unsorted_list: {}, size: {}, cycle: {}".format(sum(time_unsorted), num, cycle))
+    print("\tTotal time for linear search on sorted_list: {}, size: {}, cycle: {}".format(sum(time_sorted), num, cycle))
+
+    return sum(time_unsorted), sum(time_sorted)
+
+# test2: running linear and binary search on a sorted list
+def run_test2(num, cycle = 1000):
+    sorted_list = sorted(generate_list(num))
+
+    print("Running linear and binary search on a sorted list...")
+    time_linear = []
+    time_binary = []
+    for _ in range(cycle):
+        key = choose_key(sorted_list)
+
+        start = time.time()
+        index = search(sorted_list, key, 'linear')
+        end = time.time()
+        time_linear.append(end - start)
+
+        # print("Running binary search on sorted_list1...")
+        start = time.time()
+        index = search(sorted_list, key, 'binary')
+        end = time.time()
+        time_binary.append(end - start)
+
+    print("\tTotal time for linear search on sorted_list: {}, size: {}, cycle: {}".format(sum(time_linear), num, cycle))
+    print("\tTotal time for binary search on sorted_list: {}, size: {}, cycle: {}".format(sum(time_binary), num, cycle))
+
+    return sum(time_linear), sum(time_binary)
+
+def graphing(test1, test2):
+    # plot the results
+    plt.subplot(1, 2, 1)
+    plt.plot(size, test1["unsorted"], label = "linear search on unsorted list")
+    plt.plot(size, test1["sorted"], label = "linear search on sorted list")
+    plt.xlabel("size of list")
+    plt.ylabel("time (s)")
+    plt.legend()
+    plt.title("linear search on unsorted and sorted list")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(size, test2["linear"], label = "linear search on sorted list")
+    plt.plot(size, test2["binary"], label = "binary search on sorted list")
+    plt.xlabel("size of list")
+    plt.ylabel("time (s)")
+    plt.legend()
+    plt.title("linear and binary search on sorted list")
+
+    plt.suptitle("Linear and binary search on lists")
+    plt.show()
+
+# main
+if __name__ == '__main__':
+    size = [5000, 10000, 15000, 20000, 25000, 30000]
+    test1 = {"unsorted": [], "sorted": []}
+    test2 = {"linear": [], "binary": []}
+    for i in size:
+        r1, r2 = run_test1(i)
+        print("\n")
+        test1["unsorted"].append(r1)
+        test1["sorted"].append(r2)
+        r3, r4 = run_test2(i)
+        print("\n")
+        test2["linear"].append(r3)
+        test2["binary"].append(r4)
+    
+    graphing(test1, test2)
+
+
---
+++ sort.py
@@ -0,0 +1,96 @@
+import random
+import time
+from matplotlib import pyplot as plt
+
+# generate a list of random integers
+def generate_list(size):
+    l = []
+    for i in range(size):
+        l.append(random.randint(0, size))
+    return l
+
+# bubble sort
+def bubble_sort(l):
+    n = len(l)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if l[j] > l[j + 1] :
+                l[j], l[j + 1] = l[j + 1], l[j]
+# insertion sort
+def insert_selection(l):
+    
+    # selection sort
+    return None
+
+# run the sort algorithms
+def sort(l, method):
+    if method == 'bubble':
+        return bubble_sort(l)
+    elif method == 'insertion':
+        return insert_selection(l)
+    else:
+        return None
+
+# time the process
+def time_process(l, method):
+    start = time.perf_counter() # use time.per_counter() for better precision than time.time()
+    sort(l, method)
+    end = time.perf_counter()
+    return end - start
+
+def run_test(num, cycle = 6):
+    unsorted_list = generate_list(num) # average case
+    reverse_list = generate_list(num) # worst case
+    sorted_list = generate_list(num) # best case
+
+    result = {"reverse_bubble": [],
+                "reverse_insertion": [],
+                "sorted_bubble": [],
+                "sorted_insertion": [],
+                "unsorted_bubble": [],
+                "unsorted_insertion": []}
+
+    for _ in range(cycle):
+        # test1: running bubble sort on a reversely sorted list
+        result["reverse_bubble"].append(time_process(reverse_list, 'bubble'))
+
+        # test2: running insertion sort on a reversely sorted list
+        result["reverse_insertion"].append(time_process(reverse_list, 'insertion'))
+
+        # test3: running bubble sort on a sorted list
+        result["sorted_bubble"].append(time_process(sorted_list, 'bubble'))
+
+        # test4: running insertion sort on a sorted list
+        result["sorted_insertion"].append(time_process(sorted_list, 'insertion'))
+
+        # test5: running bubble sort on an unsorted list
+        result["unsorted_bubble"].append(time_process(unsorted_list, 'bubble'))
+
+        # test6: running insertion sort on an unsorted list
+        result["unsorted_insertion"].append(time_process(unsorted_list, 'insertion'))
+
+    return result
+
+
+if __name__ == '__main__':
+    size = [500, 1000, 1500, 2000, 2500, 3000]
+    results = []
+    for i in size:
+        results.append(run_test(i))
+
+    # plot the results
+    plt.figure(figsize=(10, 10))
+    plt.title("Time Complexity of Sorting Algorithms")
+    plt.xlabel("Size of List")
+    plt.ylabel("Time (s)")
+    plt.plot(size, results[0]["reverse_bubble"], label="reverse_bubble")
+    plt.plot(size, results[0]["reverse_insertion"], label="reverse_insertion")
+    plt.plot(size, results[0]["sorted_bubble"], label="sorted_bubble")
+    plt.plot(size, results[0]["sorted_insertion"], label="sorted_insertion")
+    plt.plot(size, results[0]["unsorted_bubble"], label="unsorted_bubble")
+    plt.plot(size, results[0]["unsorted_insertion"], label="unsorted_insertion")
+    plt.legend()
+    plt.show()
+
+    
